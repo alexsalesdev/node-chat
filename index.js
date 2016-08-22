cd334fba89a3d79e0cf9c37b8fad4806d73f5b73
Alex Sales <alexsalesdev@gmail.com>
2016-08-23 00:51:22 +0800

    added create, join and private chat

--- index.js
+++ index.js
@@ -7,21 +7,44 @@ app.get('/', function(req, res){
 });
 
 var clients = {};
+var created_rooms = {};
+var sh = require("shorthash");
+
+
 
 io.on('connection', function(socket){
     console.log('a user connected');
 
     socket.on('login', function(username){
-        clients[socket.id] = username;
-        io.emit('login.success', JSON.stringify({"id": socket.id, "name": username}));
+        clients[socket.id] = {"username" : username, "socket": socket};
+        io.emit('login.success', JSON.stringify({"id": socket.id, "username": username}));
         io.emit('chat message', username + " has logged in.");
+    });
+
+    socket.on('create', function(){
+        console.log("create room");
+        var roomId = sh.unique(socket.id);
+        created_rooms[roomId] = socket.id;
+        console.log("roomId: " + roomId);
+        io.emit('create.success', roomId);
+        io.emit('chat message', clients[socket.id].username + " created a room - " + roomId);
+    });
+
+    socket.on('join', function(roomId){
+        console.log("joining roomId: " + roomId);
+        var serverSocketId = created_rooms[roomId];
+        var serverSocket = clients[serverSocketId].socket;
+        serverSocket.emit('join.success',  {"id": serverSocketId, "username": clients[serverSocketId].username});
+        io.emit('chat message', clients[socket.id].username + " has joined a room - " + clients[serverSocketId].username);
     });
 
     socket.on('disconnect', function(){
         console.log('user disconnected');
-        var username = clients[socket.id];
-        clients[socket.id] = undefined;
-        io.emit('chat message', username + " has been disconnected.");
+        if (clients[socket.id]) {
+            var username = clients[socket.id].username;
+            delete clients[socket.id];
+            io.emit('chat message', username + " has been disconnected.");
+        }
     });
 
     socket.on('chat message', function(msg){
@@ -33,6 +56,19 @@ io.on('connection', function(socket){
         }
 
         io.emit('chat message', username + ": " + msg);
+    });
+
+    socket.on('chat privately', function(str_data){
+        var data = JSON.parse(str_data);
+        var client = clients[data.socketId];
+        var username;
+        if (clients[socket.id].username) {
+            username = clients[socket.id].username;
+        } else {
+            username = "anonymous";
+        }
+
+        client.socket.emit('chat message', "[private: " + username + "]: " + msg);
     });
 });
 
